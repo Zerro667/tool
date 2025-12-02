d22952f87e39aad44e914704e9fbefa7f199fa03
Zerro667 <topmc5022@gmail.com>
2025-12-02 03:06:34 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,19 @@
+CyberTool/
+│
+├── src/
+│   ├── main.py
+│   ├── login.py
+│   ├── phone_checker.py
+│   ├── instagram_osint.py
+│   ├── utils.py
+│   ├── banner.txt
+│   └── database.json
+│
+├── assets/
+│   ├── logo.png
+│   ├── splash.mp4
+│   └── bigfile.bin   (ملف ثقيل لرفع الحجم)
+│
+├── requirements.txt
+├── README.md
+└── install.sh
